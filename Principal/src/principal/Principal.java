5a50307be793b803d5b6a3e7efd1b5b87614fbb4
lautre <lautre@ubuntu>
2022-05-02 17:42:40 +0200

    Renombrar vae¡riable "asig" a "lista_asignaturas"

--- Principal/src/principal/Principal.java
+++ Principal/src/principal/Principal.java
@@ -15,12 +15,12 @@ public class Principal {
     public static void main(String[] args) {
         // TODO code application logic here
         //MatriculaDB matriculaDB = new MatriculaDB();
-        ArrayList asig=new ArrayList();
+        ArrayList lista_asignaturas=new ArrayList();
         Estudiant juan = new Estudiant ("Juan", "Perez", "21981534L", 2098);
-        asig.add(new Assignatura(12, "asig1", 250, 65, true));
-        asig.add(new Assignatura(15, "asig2", 190, 80, false));
-        Matricula matricula = new Matricula(2165, juan, 2, asig);
-        System.out.print("Cost de la matricula " + matricula.costMatricula());
+        lista_asignaturas.add(new Assignatura(12, "asig1", 250, 65, true));
+        lista_asignaturas.add(new Assignatura(15, "asig2", 190, 80, false));
+        Matricula matricula = new Matricula(2165, juan, 2, lista_asignaturas);
+        System.out.println("Cost de la matricula " + matricula.costMatricula());
     }
     
 }
